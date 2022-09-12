9b47273e3bcad15f32dd71a7c7a3a3c4ba6ad25b
SeanHub2021 <shbristol12@gmail.com>
2022-09-12 13:42:18 +0000

    add click listener for user input, computerChoice math for computer input, and index for results combinations

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+// Wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button")
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            
+                let gameType = this.getAttribute("data-type");
+                // alert(`You selected ${gameType}`)
+                playGame(gameType)
+            
+        })
+    }
+})
+
+//display the user & computers choice in the result area as text
+const resultDisplay = document.querySelector('#result-area')
+//enter a const for each of the choices as an array
+const choices =['rock', 'paper', 'scissors', 'lizard', 'spock']
+//when a user clicks a choice, generate a computer result, and combine the results as text value to check result
+
+
+const playGame = (userChoice) => {
+    // const userChoice = e.currentTarget.dataset.type;
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    getResults(userChoice, computerChoice)
+}
+
+
+//collect user choice + computer generated result & combine the results as text values & display the results in text 
+//in the index text area
+const getResults = (userChoice, computerChoice) => {
+    switch (userChoice + computerChoice) {
+        case 'rocklizard':
+        case 'rockscissors':
+        case 'paperrock':
+        case 'paperspock':
+        case 'scissorspaper':
+        case 'scissorslizard':
+        case 'lizardpaper':
+        case 'lizardspock':
+        case 'spockrock':
+        case 'spockscissors':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' YOU WIN!'
+            break
+        case 'lizardrock':
+        case 'scissorsrock':
+        case 'rockpaper':
+        case 'spockpaper':
+        case 'paperscissors':
+        case 'lizardscissors':
+        case 'paperlizard':
+        case 'spocklizard':
+        case 'rockspock':
+        case 'scissorsspock':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' YOU LOSE!'           
+            break
+        case 'rockrock':
+        case 'scissorscissors':
+        case 'paperpaper':
+        case 'lizardlizard':
+        case 'spockspock':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' YOU DRAW!'
+    }
+}
+
+
+// Add result to results area & player score
+function letsPlay(event) {
+    //get the Button choices into an array
+    let buttonChoices = document.getElementsByClassName('btn--big')[0];
+    //Set up the array to hold the move choice
+    let computerMove = [];
+    //iterate through the choices
+    for (let buttonChoice of buttonChoices) {
+        Math.floor(Math.random() * 4) +1
+        buttonChoice.push(computerMove);
+    }
+
+}
+
+//Tracks the player & computers score
+function incrementScore() {
+    
+}
+
+//Player lost round
+function youLostRound() {
+    
+}
+
+//Player won round
+function youWonRound() {
+}
+
+// Best of 5
+// Track scores for user or computer, first to 5 wins wins the game overall
+// Give output message to 'Result Area' in index.html
+function gameWinner() {
+
+}
