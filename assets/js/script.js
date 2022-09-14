f44cdc4e66eae8ab77df3a7c89eb764954afec0a
SeanHub2021 <shbristol12@gmail.com>
2022-09-14 16:33:13 +0000

    fix css code bugs, fix js code bugs; missing commas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
+/*jshint esversion: 6 */
 //display the user & computers choice in the result area as text
-const resultDisplay = document.getElementById('result-area')
+const resultDisplay = document.getElementById('result-area');
 //enter a const for each of the choices as an array
-const choices =['rock', 'paper', 'scissors', 'lizard', 'spock']
+const choices =['rock', 'paper', 'scissors', 'lizard', 'spock'];
 //when a user clicks a choice, generate a computer result,
 const playGame = (userChoice) => {
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
-    getResults(userChoice, computerChoice)
-}
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    getResults(userChoice, computerChoice);
+};
 //create result values for each game outcome, to change incrementally with results
 let playerWins = 0;
 let gameDraw = 0;
@@ -14,8 +15,8 @@ let computerWins = 0;
 
 //change the relevant score elements as per the user input & game outcome
 let playerScore = document.getElementById('player-score');
-let computerScore = document.getElementById('computer-score')
-let drawScore = document.getElementById('game-draw')
+let computerScore = document.getElementById('computer-score');
+let drawScore = document.getElementById('game-draw');
 
 
 //collect user choice + computer generated result & combine the results as text values & display the results in text 
@@ -33,11 +34,11 @@ const getResults = (userChoice, computerChoice) => {
         case 'lizardspock':
         case 'spockrock':
         case 'spockscissors':
-            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU WIN!'
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU WIN!';
             //if player & computer results are as above, incrementally increase the playerWins score, and insert to the
             //playerWins const, which updates the player-score id in index.html
             playerScore.innerHTML = ++playerWins;
-            break
+            break;
         case 'lizardrock':
         case 'scissorsrock':
         case 'rockpaper':
@@ -50,27 +51,27 @@ const getResults = (userChoice, computerChoice) => {
         case 'scissorsspock':
             //if player & computer results are as above, incrementally increase the computerWins score, and insert to the
             //computerWins const, which updates the computer-score id in index.html
-            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU LOSE!'           
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU LOSE!';
             computerScore.innerHTML = ++computerWins;
-            break
+            break;
         case 'rockrock':
         case 'scissorscissors':
         case 'paperpaper':
         case 'lizardlizard':
         case 'spockspock':
-            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU DRAW!'
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + '<br><br> YOU DRAW!';
             drawScore.innerHTML= ++gameDraw;
         }
-}
+};
 
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button")
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {            
                 let gameType = this.getAttribute("data-type");
-                playGame(gameType)            
-        })
+                playGame(gameType);            
+        });
     }
-})
+});
