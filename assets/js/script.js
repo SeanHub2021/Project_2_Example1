c06b3149bcbde0a276905f0392a30ecb4c99cf26
SeanHub2021 <shbristol12@gmail.com>
2022-09-15 08:49:13 +0000

    make logo reactive to page size, make text sizes reactive to page size

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,7 @@ const getResults = (userChoice, computerChoice) => {
         }
 };
 
+
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
